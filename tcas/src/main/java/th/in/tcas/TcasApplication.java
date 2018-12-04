8e719c947718ad449aa0c2c25d1cd148072a46a1
= <supawit.ruen@mail.kmutt.ac.th>
2018-12-04 17:14:48 +0700

    [ADD] init project

---
+++ tcas/src/main/java/th/in/tcas/TcasApplication.java
@@ -0,0 +1,12 @@
+package th.in.tcas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcasApplication.class, args);
+	}
+}
